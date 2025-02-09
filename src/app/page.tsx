a87263682a296dbad7430ae841ea006f9fc8cb22
Ronald Aarnoutse <ronaldaarnoutse@MacBook-Pro-van-Ronald.local>
2025-02-09 15:49:16 +0100

    start all over again with and make use of bootstrap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <main>
+        <button className="btn btn-primary">primary</button>
+        <button className="btn btn-secondary">secondary</button>
+        <button className="btn btn-warning">warning</button>
+        <button className="btn btn-danger">danger</button>
+      </main>
+      <footer></footer>
     </div>
   );
 }
